896d0a0db127d14616fb9a28dfd94147e589f30d
MicroBlaster <dmc73120@gmail.com>
2022-04-24 09:56:29 -0500

    Initial commit

---
+++ Source/Client/Controls/ResizingAdorner.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ResizeAdorner
+{
+
+    public class MoveThumb : Thumb
+    {
+        private RotateTransform rotateTransform;
+        private ContentControl designerItem;
+
+        public MoveThumb()
+        {
+            DragStarted += new DragStartedEventHandler(this.MoveThumb_DragStarted);
+            DragDelta += new DragDeltaEventHandler(this.MoveThumb_DragDelta);
+        }
+
+        private void MoveThumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            this.designerItem = DataContext as ContentControl;
+
+            if (this.designerItem != null)
+            {
+                this.rotateTransform = this.designerItem.RenderTransform as RotateTransform;
+            }
+        }
+
+        private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (this.designerItem != null)
+            {
+                Point dragDelta = new Point(e.HorizontalChange, e.VerticalChange);
+
+                if (this.rotateTransform != null)
+                {
+                    dragDelta = this.rotateTransform.Transform(dragDelta);
+                }
+
+                Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + dragDelta.X);
+                Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + dragDelta.Y);
+            }
+        }
+    }
+
+    public class ResizeThumb : Thumb
+    {
+        private RotateTransform rotateTransform;
+        private double angle;
+        private Adorner adorner;
+        private Point transformOrigin;
+        private ContentControl designerItem;
+        private Canvas canvas;
+
+        public ResizeThumb()
+        {
+            DragStarted += new DragStartedEventHandler(this.ResizeThumb_DragStarted);
+            DragDelta += new DragDeltaEventHandler(this.ResizeThumb_DragDelta);
+            DragCompleted += new DragCompletedEventHandler(this.ResizeThumb_DragCompleted);
+        }
+
+        private void ResizeThumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            this.designerItem = this.DataContext as ContentControl;
+
+            if (this.designerItem != null)
+            {
+                this.canvas = VisualTreeHelper.GetParent(this.designerItem) as Canvas;
+
+                if (this.canvas != null)
+                {
+                    this.transformOrigin = this.designerItem.RenderTransformOrigin;
+
+                    this.rotateTransform = this.designerItem.RenderTransform as RotateTransform;
+                    if (this.rotateTransform != null)
+                    {
+                        this.angle = this.rotateTransform.Angle * Math.PI / 180.0;
+                    }
+                    else
+                    {
+                        this.angle = 0.0d;
+                    }
+
+                    AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.canvas);
+                    if (adornerLayer != null)
+                    {
+                        this.adorner = new SizeAdorner(this.designerItem);
+                        adornerLayer.Add(this.adorner);
+                    }
+                }
+            }
+        }
+
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (this.designerItem != null)
+            {
+                double deltaVertical, deltaHorizontal;
+
+                switch (VerticalAlignment)
+                {
+                    case System.Windows.VerticalAlignment.Bottom:
+                        deltaVertical = Math.Min(-e.VerticalChange, this.designerItem.ActualHeight - this.designerItem.MinHeight);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle));
+                        this.designerItem.Height -= deltaVertical;
+                        break;
+                    case System.Windows.VerticalAlignment.Top:
+                        deltaVertical = Math.Min(e.VerticalChange, this.designerItem.ActualHeight - this.designerItem.MinHeight);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + deltaVertical * Math.Cos(-this.angle) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + deltaVertical * Math.Sin(-this.angle) - (this.transformOrigin.Y * deltaVertical * Math.Sin(-this.angle)));
+                        this.designerItem.Height -= deltaVertical;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (HorizontalAlignment)
+                {
+                    case System.Windows.HorizontalAlignment.Left:
+                        deltaHorizontal = Math.Min(e.HorizontalChange, this.designerItem.ActualWidth - this.designerItem.MinWidth);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + deltaHorizontal * Math.Cos(this.angle) + (this.transformOrigin.X * deltaHorizontal * (1 - Math.Cos(this.angle))));
+                        this.designerItem.Width -= deltaHorizontal;
+                        break;
+                    case System.Windows.HorizontalAlignment.Right:
+                        deltaHorizontal = Math.Min(-e.HorizontalChange, this.designerItem.ActualWidth - this.designerItem.MinWidth);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + (deltaHorizontal * this.transformOrigin.X * (1 - Math.Cos(this.angle))));
+                        this.designerItem.Width -= deltaHorizontal;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void ResizeThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (this.adorner != null)
+            {
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.canvas);
+                if (adornerLayer != null)
+                {
+                    adornerLayer.Remove(this.adorner);
+                }
+
+                this.adorner = null;
+            }
+        }
+
+    }
+
+    public class RotateThumb : Thumb
+    {
+        private Point centerPoint;
+        private Vector startVector;
+        private double initialAngle;
+        private Canvas designerCanvas;
+        private ContentControl designerItem;
+        private RotateTransform rotateTransform;
+
+        public RotateThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.RotateThumb_DragDelta);
+            DragStarted += new DragStartedEventHandler(this.RotateThumb_DragStarted);
+        }
+
+        private void RotateThumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            this.designerItem = DataContext as ContentControl;
+
+            if (this.designerItem != null)
+            {
+                this.designerCanvas = VisualTreeHelper.GetParent(this.designerItem) as Canvas;
+
+                if (this.designerCanvas != null)
+                {
+                    this.centerPoint = this.designerItem.TranslatePoint(
+                        new Point(this.designerItem.Width * this.designerItem.RenderTransformOrigin.X,
+                                  this.designerItem.Height * this.designerItem.RenderTransformOrigin.Y),
+                                  this.designerCanvas);
+
+                    Point startPoint = Mouse.GetPosition(this.designerCanvas);
+                    this.startVector = Point.Subtract(startPoint, this.centerPoint);
+
+                    this.rotateTransform = this.designerItem.RenderTransform as RotateTransform;
+                    if (this.rotateTransform == null)
+                    {
+                        this.designerItem.RenderTransform = new RotateTransform(0);
+                        this.initialAngle = 0;
+                    }
+                    else
+                    {
+                        this.initialAngle = this.rotateTransform.Angle;
+                    }
+                }
+            }
+        }
+
+        private void RotateThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (this.designerItem != null && this.designerCanvas != null)
+            {
+                Point currentPoint = Mouse.GetPosition(this.designerCanvas);
+                Vector deltaVector = Point.Subtract(currentPoint, this.centerPoint);
+
+                double angle = Vector.AngleBetween(this.startVector, deltaVector);
+
+                RotateTransform rotateTransform = this.designerItem.RenderTransform as RotateTransform;
+                rotateTransform.Angle = this.initialAngle + Math.Round(angle, 0);
+                this.designerItem.InvalidateMeasure();
+            }
+        }
+    }
+
+    public class SizeAdorner : Adorner
+    {
+        private SizeChrome chrome;
+        private VisualCollection visuals;
+        private ContentControl designerItem;
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return this.visuals.Count;
+            }
+        }
+
+        public SizeAdorner(ContentControl designerItem)
+            : base(designerItem)
+        {
+            this.SnapsToDevicePixels = true;
+            this.designerItem = designerItem;
+            this.chrome = new SizeChrome();
+            this.chrome.DataContext = designerItem;
+            this.visuals = new VisualCollection(this);
+            this.visuals.Add(this.chrome);
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return this.visuals[index];
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            this.chrome.Arrange(new Rect(new Point(0.0, 0.0), arrangeBounds));
+            return arrangeBounds;
+        }
+    }
+
+    public class SizeChrome : Control
+    {
+        static SizeChrome()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(SizeChrome), new FrameworkPropertyMetadata(typeof(SizeChrome)));
+        }
+    }
+
+    public class DoubleFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double d = (double)value;
+            return Math.Round(d);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+
+}
---
+++ Source/Client/FirstMate/Main.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstMate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Main: Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right)
+            {
+                if (Selector.GetIsSelected((Control)sender))
+                {
+                    Selector.SetIsSelected((Control)sender, false);
+                }
+                else
+                {
+                    Selector.SetIsSelected((Control)sender, true);
+                }
+
+            }
+
+        }
+    }
+}
