9031592d5accb5f35e9097842567b92f04a72310
MicroBlaster <dmc73120@gmail.com>
2022-11-10 16:37:12 -0600

    .

---
+++ Source/Shared/Configuration/Data/ComfigContext.cs
@@ -0,0 +1,34 @@
+﻿using Configuration.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Configuration.Data;
+
+public class ConfigContext : DbContext
+{
+    const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;" +
+        "Trusted_Connection=True;Database=FirstMate-Config";
+
+
+    public ConfigContext() : base() { }
+    //public CondigContext(DbContextOptions<CondigContext> options) : base(options) { }
+
+    //
+    public DbSet<Setting> Settings { get; set; }
+
+    //
+    public DbSet<Menu> Menus { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
---
+++ Source/Shared/Configuration/Models/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration.Models;
+
+public class MenuItem
+{
+    [Key]
+    public Guid MenuItemID { get; set; }
+    [Required]
+    public Guid ParentID { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Key { get; set; }
+    [Required]
+    public string? Caption { get; set; }
+    public string? Command { get; set; }
+    public string? Parameters { get; set; }
+    public string? Help { get; set; }
+}
